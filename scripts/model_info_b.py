62a1e8cc080d78b45a84459b86102dff3617b146
Dongju Yeon <>
2026-04-28 10:20:35 +0900

    fix: yaml file name change, 'detail = true' deleted

--- scripts/model_info_b.py
+++ scripts/model_info_b.py
@@ -5,7 +5,8 @@ def main():
     model = YOLO("configs/yolov8n_b_condconv_frelu.yaml")
 
     print("========== MODEL INFO ==========")
-    model.info(detailed=True, verbose=True)
+    print(model.model)
+    model.info()
     print("================================")
 
 
